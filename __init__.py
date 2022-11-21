860f428489a02c9997462bf2ab145b970aaa1f96
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-11-21 10:05:23 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -20,6 +20,15 @@ def classFactory(iface):
     new_title = title.replace('QGIS', 'TABULA')
     iface.mainWindow().setWindowTitle(new_title)
     iface.mainWindow().statusBar().showMessage(time.asctime())
+    
+    iface.mainWindow().blockSignals(True)
+    self.crs = QgsCoordinateReferenceSystem()
+    self.crs.createFromProj4("+proj=lcc +lat_1=33.0 +lat_2=60.0 +lat_0=40.0 +lon_0=-97.0 +x_0=-792000.0 +y_0=1080000.0 +datum=WGS84 +no_defs")
+    self.crs.saveAsUserCrs("TABULA2 CRS")
+    QgsProject.instance().setCrs(self.crs)
+    iface.mainWindow().blockSignals(False)
+#    QApplication.processEvents()
+
 #    iface.mainWindow().removeToolBar(toolbar)
 #    vector_menu = iface.vectorMenu()
 #    raster_menu = iface.rasterMenu()
