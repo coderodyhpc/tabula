616894bd2cda1ff8d9fcb032228a2bcc75974517
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-11-21 14:00:04 -0500

    Update mainPlugin.py

--- plugin/mainPlugin.py
+++ plugin/mainPlugin.py
@@ -25,13 +25,13 @@ class TabulaDock(QDockWidget):
         self.tabs = tabs
         self.add_stamen_basemap()
         self.crs = QgsCoordinateReferenceSystem()
-        aaa = self.crs.createFromProj4("+proj=lcc +lat_1=33.0 +lat_2=60.0 +lat_0=40.0 +lon_0=-97.0 +x_0=-792000.0 +y_0=1080000.0 +datum=WGS84 +no_defs")
-        print ("AAA ",aaa)
+        self.crs.createFromProj4("+proj=lcc +lat_1=33.0 +lat_2=60.0 +lat_0=40.0 +lon_0=-97.0 +x_0=-792000.0 +y_0=1080000.0 +datum=WGS84 +no_defs")
         self.crs.saveAsUserCrs("TABULA CRS")
 #        QApplication.processEvents()
-        bbb = QgsProject.instance().setCrs(self.crs)
-        print ("BBB ",bbb)
-        QApplication.processEvents()
+        QgsProject.instance().setCrs(self.crs)
+#        print ("BBB ",bbb)
+        iface.mapCanvas().refresh()
+#        QApplication.processEvents()
         print ("MAP CANVAS ",iface.mapCanvas())
 #        self.add_naip_basemap()
         
