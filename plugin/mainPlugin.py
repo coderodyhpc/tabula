1473e4f7653821369c3a48d3310554ccccee7346
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-12-06 13:54:32 -0500

    Update mainPlugin.py

--- plugin/mainPlugin.py
+++ plugin/mainPlugin.py
@@ -26,7 +26,8 @@ class TabulaDock(QDockWidget):
         self.add_stamen_basemap()
         for layer in QgsProject.instance().mapLayers().values():
             print ("IT IS AT LAYER ",layer)
-            layer.setCrs(QgsCoordinateReferenceSystem('EPSG:4979'))
+#            layer.setCrs(QgsCoordinateReferenceSystem('EPSG:4979'))
+            layer.setCrs(QgsCoordinateReferenceSystem('ESRI:102004'))
 #        self.add_naip_basemap()
         
     def add_stamen_basemap(self):
