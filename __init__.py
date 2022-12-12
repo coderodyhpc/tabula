9e82e14165c7842239ca0333f8f48892f9430682
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-12-12 18:42:58 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -40,13 +40,13 @@ def classFactory(iface):
     
     command1 = ['ec2metadata', '--instance-id']
     proc1 = subprocess.Popen(command1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    EC2_INSTANCE_ID = proc1.communicate()
+    EC2_INSTANCE_ID, error1 = proc1.communicate()
     command2 = ['ec2metadata', '--instance-type']
     proc2 = subprocess.Popen(command2, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    EC2_INSTANCE_TYPE = proc2.communicate()
+    EC2_INSTANCE_TYPE, error2 = proc2.communicate()
     command3 = ['ec2metadata', '--availability-zone']
     proc3 = subprocess.Popen(command3, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    EC2_ZONE = proc3.communicate()
+    EC2_ZONE, error3 = proc3.communicate()
 #    temp = subprocess.Popen([ec2metadata, '--instance-id'], stdout = subprocess.PIPE)
     print ("EC2_INSTANCE_ID ",EC2_INSTANCE_ID)
     print ("EC2_INSTANCE_TYPE ",EC2_INSTANCE_TYPE)
