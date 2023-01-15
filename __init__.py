5cade2873a5c7af563de0bb1d5684885a12a0293
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2023-01-15 18:40:39 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import subprocess
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtWidgets import QToolBar, QPushButton, Qwidget
+from PyQt5.QtWidgets import QToolBar, QPushButton, QWidget
 
 def classFactory(iface):
     """Load QGISPlugin class.
