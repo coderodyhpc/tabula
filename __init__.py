5ce7375e2521e0819a751b55dd4ab89fbc4645f2
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2023-01-15 12:21:31 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,6 +32,43 @@ def classFactory(iface):
 #    menubar.removeAction(web_menu.menuAction())
 #    menubar.removeAction(processing_menu.menuAction())
 #    menubar.addAction(dummy_menu)
+
+    command1 = ['ec2metadata', '--instance-id']
+    proc1 = subprocess.Popen(command1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    EC2_INSTANCE_IDx = proc1.communicate()[0].decode("utf-8")
+    EC2_INSTANCE_ID = EC2_INSTANCE_IDx.strip() 
+    command2 = ['ec2metadata', '--instance-type']
+    proc2 = subprocess.Popen(command2, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    EC2_INSTANCE_TYPEx = proc2.communicate()[0].decode("utf-8")
+    EC2_INSTANCE_TYPE = EC2_INSTANCE_TYPEx.strip() 
+    command3 = ['ec2metadata', '--availability-zone']
+    proc3 = subprocess.Popen(command3, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    EC2_ZONEx = proc3.communicate()[0].decode("utf-8")
+    EC2_ZONE = EC2_ZONEx.strip() 
+    print ("THESE ARE THE EC2 VARIABLES ",EC2_INSTANCE_ID,EC2_INSTANCE_TYPE,EC2_INSTANCE_ZONE)
+    
+    lscpu_nomen = ((subprocess.check_output("lscpu", shell=True).strip()).decode())
+    for item in lscpu_nomen.split("\n"):
+        if "Model name" in item:
+            modeln_1 = item.strip()
+    modeln_2 = modeln_1.replace("Model name:","")    
+    cpu_nomen = modeln_2.replace(" ","")  
+    if cpu_nomen == 'Neoverse-N1':
+        nomen2 = "Graviton2"
+    elif cpu_nomen == '1':
+        nomen2 = "Graviton3"
+    else:    
+        nomen2 = cpu-nomen
+    cpu_NM = "CPU: "+nomen2+"\n"
+    cpu_NM2 = EC2_INSTANCE_TYPE
+#    cpu_NM2.replace("\n","")
+    cpu_NM3 = EC2_ZONE
+    cpu_TOTAL = cpu_NM+cpu_NM2 
+    odyimum = QPushButton(cpu_TOTAL) 
+    odyimum.setStyleSheet(".QPushButton {color: black; background-color:#FF9900;}")
+    odyimum.setFont(QFont('Verdana', 10))
+    iface.mainWindow().statusBar().addWidget(odyimum)    
+
     return QGISPlugin(iface)
 
 def dummy_menu():
