02770ba4461af684735f13b51a2b5db99d64eb15
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2023-01-29 10:55:37 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,10 +3,10 @@ from qgis.core import QgsCoordinateReferenceSystem, QgsProject
 def classFactory(iface):
 
     from tabula.plugin.mainPlugin import QGISPlugin
-    novus_CRS = '+proj=lcc +lat_1=63.0 +lat_2=75.0 +lat_0=70.0 +lon_0=-97.0 +datum=WGS84 +no_defs'
-    crs = QgsCoordinateReferenceSystem.fromProj4(novus_CRS)
-    qgsProject = QgsProject.instance() 
-    qgsProject.setCrs(crs)
+#    novus_CRS = '+proj=lcc +lat_1=63.0 +lat_2=75.0 +lat_0=70.0 +lon_0=-97.0 +datum=WGS84 +no_defs'
+#    crs = QgsCoordinateReferenceSystem.fromProj4(novus_CRS)
+#    qgsProject = QgsProject.instance() 
+#    qgsProject.setCrs(crs)
     return QGISPlugin(iface)
 
 def dummy_menu():
