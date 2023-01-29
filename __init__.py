611de14c33394ac1d7e4e405d355d6986c1b488e
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2023-01-29 09:12:59 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,37 +1,11 @@
+from qgis.core import QgsCoordinateReferenceSystem, QgsProject
+
 def classFactory(iface):
-    """Load QGISPlugin class.
-    Parameters
-    ----------
-    iface: qgis.gui.QgisInterface
-        An interface instance that will be passed to this class
-        which provides the hook by which you can manipulate the QGIS
-        application at run time.
-    Returns
-    -------
-    """
 
     from tabula.plugin.mainPlugin import QGISPlugin
-#    project = QgsProject.instance()
-#    project.setTitle('HOLAAAA')
-#    project.write()
-    title = iface.mainWindow().windowTitle()
-    new_title = title.replace('QGIS', 'TABULA')
-    iface.mainWindow().setWindowTitle(new_title)
-#    iface.mainWindow().removeToolBar(toolbar)
-    vector_menu = iface.vectorMenu()
-#    raster_menu = iface.rasterMenu()
-#    mesh_menu = iface.meshMenu()
-#    database_menu = iface.databaseMenu()
-#    web_menu = iface.webMenu()
-#    processing_menu = iface.processingMenu()
-    menubar = vector_menu.parentWidget()
-#    menubar.removeAction(raster_menu.menuAction())
-    menubar.removeAction(vector_menu.menuAction())
-#    menubar.removeAction(database_menu.menuAction())
-#    menubar.removeAction(mesh_menu.menuAction())
-#    menubar.removeAction(web_menu.menuAction())
-#    menubar.removeAction(processing_menu.menuAction())
-#    menubar.addAction(dummy_menu)
+    novus_CRS = '+proj=lcc +lat_1=33.0 +lat_2=60.0 +lat_0=40.0 +lon_0=-97.0 +x_0=-792000.0 +y_0=1080000.0 +datum=WGS84 +no_defs'
+    qgsProject = QgsProject.instance() 
+    qgsProject.setCrs(novus_CRS)
     return QGISPlugin(iface)
 
 def dummy_menu():
