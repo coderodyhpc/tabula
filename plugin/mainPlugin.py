4fad325c9cdd0fbb4392a9e674ce714aefd975c6
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2023-04-03 10:29:49 -0400

    Update mainPlugin.py

--- plugin/mainPlugin.py
+++ plugin/mainPlugin.py
@@ -99,18 +99,23 @@ class Legenda(QgsMapCanvasItem):
         imum_sinister = [400, 400]
         painter.setPen(QColor(Qt.black))
         painter.drawRect(imum_sinister[0]-5, imum_sinister[1]-5-(9*self.altitudo+10), 100, 9*self.altitudo+20)
-#        for iii in range(3):
+        for iii in range(3):
 #            print ("III ",iii,self.numeri[iii],self.pigmenti[iii])
-#            painter.setPen(QColor(Qt.black))
-#            painter.setFont(QFont('Verdana', self.altitudo-2))
-#            painter.drawText(imum_sinister[0]+self.longitudo+5, imum_sinister[1]-1+(iii*self.altitudo), self.numeri[iii])
-#            painter.fillRect(imum_sinister[0], imum_sinister[1]+((iii+1)*self.altitudo), self.longitudo, self.altitudo, QColor(self.pigmenti[iii]))          
+            painter.setPen(QColor(Qt.black))
+            painter.setFont(QFont('Verdana', self.altitudo-2))
+            painter.drawText(imum_sinister[0]+self.longitudo+5, imum_sinister[1]-1+(iii*self.altitudo), self.numeri[iii])
+            if iii == 0:
+                painter.fillRect(imum_sinister[0], imum_sinister[1]+((iii+1)*self.altitudo), self.longitudo, self.altitudo, QColor(255,0,0))          
+            elif iii == 1:
+                painter.fillRect(imum_sinister[0], imum_sinister[1]+((iii+1)*self.altitudo), self.longitudo, self.altitudo, QColor(199,0,0))          
+            elif iii == 2:
+                painter.fillRect(imum_sinister[0], imum_sinister[1]+((iii+1)*self.altitudo), self.longitudo, self.altitudo, QColor(199,0,0))          
 #        aaa = QColor(self.pigmenti[0])
-        print ("PIGM ",self.pigmenti[0],type(self.pigmenti[0]))
-        painter.setPen(QColor(Qt.black))
-        painter.setFont(QFont('Verdana', self.altitudo-2))
-        painter.drawText(imum_sinister[0]+self.longitudo+5, imum_sinister[1]-1+(0*self.altitudo), self.numeri[0])
-        painter.fillRect(imum_sinister[0], imum_sinister[1]-(1*self.altitudo), self.longitudo, self.altitudo, self.pigmenti[0])          
+#        print ("PIGM ",self.pigmenti[0],type(self.pigmenti[0]))
+#        painter.setPen(QColor(Qt.black))
+#        painter.setFont(QFont('Verdana', self.altitudo-2))
+#        painter.drawText(imum_sinister[0]+self.longitudo+5, imum_sinister[1]-1+(0*self.altitudo), self.numeri[0])
+#        painter.fillRect(imum_sinister[0], imum_sinister[1]-(1*self.altitudo), self.longitudo, self.altitudo, self.pigmenti[0])          
             
 #        painter.drawRect(100, 100, 100, 50)
         print ("After painter")
