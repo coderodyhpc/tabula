15c424ff4533adec260e1d49da15f4b7099af079
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2023-08-07 14:30:58 -0400

    Update emissions.py

--- plugin/emissions.py
+++ plugin/emissions.py
@@ -1,10 +1,35 @@
-from PyQt5.QtWidgets import QDockWidget, QTabWidget
+from PyQt5.QtWidgets import QDockWidget, QTabWidget, QPushButton, QVBoxLayout, QFileDialog, QStringList
 from PyQt5.QtCore import pyqtSignal, QEvent
 from PyQt5.QtGui import QMouseEvent, QColor
 
-class Emissions(QDockWidget):
-    def __init__(self, iface: QgisInterface, dock_widget: QDockWidget) -> None:   
+class Emissions(QWidget):
+    tab_active = pyqtSignal()
+    def __init__(self, iface) -> None:   
         super().__init__('Emissions')
         print ("EMISSIONS")
-                
         
+        self.iface = iface
+        self.times = []
+        self.vbox = QVBoxLayout()
+        self.fileOpenButton = QPushButton('Click to open emissions file',self)
+        self.fileOpenButton.clicked.connect(self.getncfiles)
+        self.vbox.addWidget(self.fileOpenButton)
+        
+        self.setLayout(self.vbox)
+        
+    def buttonClicked(self):
+        wig = App()
+                
+    def getncfiles(self):
+        dlg = QFileDialog()
+        dlg.setFileMode(QFileDialog.AnyFile)
+        dlg.setFilter("nc files (*.nc)")
+        filenames = QStringList()
+		
+        if dlg.exec_():
+            filenames = dlg.selectedFiles()
+            f = open(filenames[0], 'r')
+			
+            with f:
+                data = f.read()
+                self.contents.setText(data)        
